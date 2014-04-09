b49b135bfcecf3c4933d578e49a1c9ce3efbf430
Reid Cohen <rac336@cornell.edu>
2014-04-08 23:27:19 -0400

    added a test class

---
+++ Git Test/RCTest.h
@@ -0,0 +1,13 @@
+//
+//  RCTest.h
+//  Git Test
+//
+//  Created by Reid Cohen on 4/8/14.
+//  Copyright (c) 2014 Reid Cohen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTest : NSObject
+
+@end
---
+++ Git Test/RCTest.m
@@ -0,0 +1,13 @@
+//
+//  RCTest.m
+//  Git Test
+//
+//  Created by Reid Cohen on 4/8/14.
+//  Copyright (c) 2014 Reid Cohen. All rights reserved.
+//
+
+#import "RCTest.h"
+
+@implementation RCTest
+
+@end
